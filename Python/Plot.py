279a2a98f3585a795af58d1edc0549d0fc0c5759
Tetat Chulchue <61070089@kmitl.ac.th>
2018-12-18 12:39:19 +0700

    Back-End #6
    + byGames.svg
    + byGenre.svg
    * Data.txt file (Remove Thai data)

--- Python/Plot.py
+++ Python/Plot.py
@@ -5,13 +5,10 @@
 import pygal as py
 import json as js
 
-# ------- Read Data File Section -------
-
-record = []
+# ------- Data Section -------
 
 with open('Data.txt') as f:
-    for line in f:
-        record.append(line.replace('\n', '').split('~'))
+    record = [line.replace('\n', '').split('~') for line in f]
 
 record[0][0] = 'N'
 
@@ -21,6 +18,26 @@ platform = [record[i][2] for i in range(len(record))]
 genre = [record[i][3].lstrip('[').rstrip(']').replace(' ', '').split(',') for i in range(len(record))]
 games = [record[i][4].lstrip('[').rstrip(']').replace(' ', '').split(',') for i in range(len(record))]
 
+genre_count = {}
+for i in genre:
+    for j in i:
+        if j not in genre_count:
+            genre_count[j] = 1
+        else:
+            genre_count[j] += 1
+
+games_count = {}
+for i in games:
+    for j in i:
+        if j not in games_count:
+            games_count[j.encode('ascii', 'ignore').decode("utf-8")] = 1
+        else:
+            games_count[j.encode('ascii', 'ignore').decode("utf-8")] += 1
+
+del games_count['MOBA']
+
+print(js.dumps(genre_count, indent=2))
+print(js.dumps(games_count, indent=2))
 
 # ------- Draw A Graph Sections --------
 
@@ -48,10 +65,23 @@ byplatform = py.Pie()
 byplatform.title = 'Platform'
 byplatform.add('PC', platform.count('PC'))
 byplatform.add('Mobile', platform.count('Mobile')+1)
-byplatform.add('Consoles', platform.count('Consoles'))
+byplatform.add('Consoles', platform.count('Consoles')+1)
 byplatform.render_to_file('./chart/byPlatform.svg')
 
 # .......................................
 
+bygenre = py.Pie()
+bygenre.title = 'Genre'
+for i in genre_count:
+    bygenre.add(i, genre_count[i])
+bygenre.render_to_file('./chart/byGenre.svg')
+
+# .......................................
+
+bygames = py.Pie()
+bygames.title = 'Games'
+for i in games_count:
+    bygames.add(i, games_count[i])
+bygames.render_to_file('./chart/byGames.svg')
 
 
